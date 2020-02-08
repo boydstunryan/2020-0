3592fb19099616f2df5eb503e2a1320e505868e7
ryanb <boydstunryan@yahoo.com>
2020-02-08 01:03:22 -0700

    Character controller class

---
+++ 2020-0/Assets/CharacterMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 positionDirection;
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            positionDirection.y = jumpForce;
+        }
+        positionDirection.y -= gravity;
+        controller.Move(motion: positionDirection * Time.deltaTime);
+    }
+}
