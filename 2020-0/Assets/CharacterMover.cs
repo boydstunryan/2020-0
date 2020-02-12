8697d5f52b0d4ef20dfa1e2a644d071812a791e4
ryanb <boydstunryan@yahoo.com>
2020-02-12 09:51:56 -0700

    changes to the controller script

--- 2020-0/Assets/CharacterMover.cs
+++ 2020-0/Assets/CharacterMover.cs
@@ -7,6 +7,9 @@ public class CharacterMover : MonoBehaviour
     public CharacterController controller;
     public Vector3 positionDirection;
     public float speed = 10f;
+    public float gravity = 3f;
+    public float jumpForce = 30f;
+    public int jumpCount = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +19,17 @@ public class CharacterMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (controller.isGrounded)
+        {
+            positionDirection.y = 0;
+            jumpCount = 0;
+        }
         positionDirection.x = Input.GetAxis("Horizontal")*speed;
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
         {
             positionDirection.y = jumpForce;
+            jumpCount++;
         }
         positionDirection.y -= gravity;
         controller.Move(motion: positionDirection * Time.deltaTime);
---
+++ 2020-0/Assets/RigidBodyForces.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyForces : MonoBehaviour
+{
+    public Rigidbody rigidBodyObj;
+    public float force = 100;
+    private Vector3 forceVector = new Vector3();
+ 
+    private void OnCollisionEnter(Collision other)
+    {
+        forceVector.y = force;
+        rigidBodyObj.AddExplosionForce(forceVector);
+    }
+
+}
