17b5cbd1a92e978ef94b0f393780f1d96006f4cc
ryanb <boydstunryan@yahoo.com>
2020-03-26 23:00:06 -0600

    Enemy attack and player health script

---
+++ 2020-0/Assets/PlayerHealth.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+    Animator anim;
+    PlayerController playerController;
+    PlayerShoot playerShoot;
+    bool isDead;
+    bool damaged;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+        playerShoot = GetComponentInChildren<PlayerShoot>();
+
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (damaged)
+        {
+            damageImage.color = flashColor;
+        }
+
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        damaged = false;
+    }
+
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+
+        healthSlider.value = currentHealth;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        playerShoot.DestroyFinishedParticle();
+
+        anim.SetTrigger("die");
+
+        playerController.enabled = false;
+        playerShoot.enabled = false;
+    }
+}
