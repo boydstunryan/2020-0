93c0962c2d44bf071d452bb8ea867163259ddfd2
ryanb <boydstunryan@yahoo.com>
2020-03-28 18:40:44 -0600

    Script for picking up coins and keeping score

---
+++ 2020-0/Assets/CoinPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+
+    public int coinValue;
+   
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Youve collect the coin!!");
+
+        ScoreManager.AddPoints(coinValue);
+
+        Destroy(gameObject);
+    }
+}
