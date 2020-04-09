fc720a5a2e0f84e71c07fccf2289dff088ee1489
ryanb <boydstunryan@yahoo.com>
2020-04-08 19:47:24 -0600

    Projectile scripts, level scripts, enemy scripts, and player shoot scrip

---
+++ 2020-0/Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public int levelToLoad;
+    // Start is called before the first frame update
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    // Update is called once per frame
+    public void LevelExit()
+    {
+        Application.Quit();
+    }
+}
