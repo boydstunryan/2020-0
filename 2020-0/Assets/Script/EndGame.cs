d9c5644f2d4f559fad8497378f5518f6ec86deeb
ryanb <boydstunryan@yahoo.com>
2020-04-16 15:54:23 -0600

    Animaton controller and UI additions

---
+++ 2020-0/Assets/Script/EndGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        void OnTriggerEnter2D(Collider other)
+        {
+            if (other.transform.tag == "EndPlatform")
+            {
+                LevelManager.LoadScene("Menu");
+            }
+
+        }
+        
+    }
+}
